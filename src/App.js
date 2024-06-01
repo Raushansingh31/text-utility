f4a71aad37a552e7b52ed36a8776d2ca0cebe828
Raushan Singh <116196411+Raushansingh31@users.noreply.github.com>
2024-06-01 17:23:45 +0530

    text utility project based on react
    this is react project which is deployed through github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AboutUs from './components/AboutUs';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import Alert from './Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message,type) =>{
+    setAlert({
+      msg : message,
+      type : type
+    })
+    setTimeout ( ()=>{
+      setAlert(null);
+    }, 1000)
+  }
+  const togglemode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor='green';
+      showAlert("Dark mode is enabled", "success");
+      document.title='text -dark';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode is enabled", "success");
+      document.title='text -light';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Router>
+        <Navbar title="TextUtils" mode={mode} togglemode={togglemode}/>
+        <Alert alert={alert}/>
+        <div className='container'>
+        <Routes>
+          <Route exact path="/about" element={<AboutUs/>}>
+          </Route>
+          <Route exact path="/" element ={<Textform showAlert={showAlert} heading="uppercase convereter" mode={mode} />}>
+          </Route>
+        </Routes>
+        {/*<Textform showAlert={showAlert} heading="uppercase convereter" mode={mode} />*/}
+        </div>
+        </Router>
+    </>
   );
 }
 
---
+++ src/components/Textform.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+
+export default function Textform(props) {
+    const handleUpClick =()=>{
+       // console.log("click is forced");
+        let newText=text.toUpperCase();
+        setText(newText);
+        props.showAlert("Uppercase is applied","success");
+    }
+    const handleLowClick =()=>{
+        //console.log("click is forced");
+        let newText=text.toLowerCase();
+        setText(newText);
+        props.showAlert("Lowercase is applied","success");
+    }
+    const handleOnChange =(event)=>{
+        console.log("onChange is forcved");
+        setText(event.target.value);
+    }
+    const [text, setText] = useState('enter text here');
+  return (
+    <div>
+      
+        <div className="mb-3">
+        <label htmlFor="myText" className="form-label">{props.heading}</label>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor : props.mode=== 'dark'? 'grey':'white' , color:props.mode=== 'dark'? 'white':'black' }} id="myText" rows="8"></textarea>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>convert uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>convert lowercase</button>
+        </div>
+        <div className="container my-3">
+            <h1>Your text summary</h1>
+            <p>{text.split(" ").length} words are there and {text.length} characteres</p>
+        </div>
+    </div>
+  )
+}
